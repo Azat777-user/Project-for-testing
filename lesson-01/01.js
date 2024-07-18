258466938b1594b843548864b4ea6dabaee2eae8
Azat Mardanov <mardanow.azat2016@yandex.ru>
2024-07-18 21:42:15 +0300

    complete task

---
+++ lesson-01/01.js
@@ -0,0 +1,13 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum = 0;
+let i = 1;
+let total = 20;
+
+while(i <= total) {
+    sum += i++;
+}
+
+console.log(sum);
