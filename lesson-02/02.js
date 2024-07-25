d6350b9f5fa268acdf15e96f0ae5abc93f1a688a
Azat Mardanov <mardanow.azat2016@yandex.ru>
2024-07-25 14:12:27 +0300

    complete task2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -9,17 +9,17 @@
 
 let dice1 // your code
 let dice2 // your code
-let isWinningDouble = false // your code
+let isWinningDouble // your code
 
 dice1 = Math.floor(1 + Math.random() * (6 + 1 - 1))
 dice2 = Math.floor(1 + Math.random() * (6 + 1 - 1))
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
-if (dice1 > 3 && dice2 > 3 ) {
+
+isWinningDouble = ((dice1 === dice2) > 3) ? true : false
+if (isWinningDouble) {
   console.log('Выигрышный дубль!')
-  isWinningDouble = true
 } else {
   console.log('Не выигрышный дубль.')
-  isWinningDouble = false
 }
 
