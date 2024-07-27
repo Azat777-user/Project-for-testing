a726909bdcf88a1353d6d826f1a3722f28c3a3db
Azat Mardanov <mardanow.azat2016@yandex.ru>
2024-07-27 17:46:04 +0300

    complete task

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -21,7 +21,7 @@ debugger
 
 let isUserAutorized = ((isAdmin || isVerifiedUser) && (hasSpecialPermission ||hasTemporaryPass)) 
 
-//isAccess = isUserAutorized ? true : false
+isAccess = isUserAutorized 
 
 // if((isAdmin || isVerifiedUser) && (hasSpecialPermission ||hasTemporaryPass)) 
 // { 
