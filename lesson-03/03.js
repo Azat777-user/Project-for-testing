6fa4af23e8d7ab4f8745327733692c36a94066c1
Azat Mardanov <mardanow.azat2016@yandex.ru>
2024-07-28 15:13:57 +0300

    complete task

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,9 +1,11 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
 function findLargest(number1, number2, number3) {
+    debugger
     let largetNumber = 0
     if (number1 > number2 && number1 > number3) largetNumber = number1
     if (number1 < number2 && number2 > number3) largetNumber = number2
     if (number3 > number1 && number2 < number3) largetNumber = number3
+    if((number1 === number2) &&(number2 === number3)) largetNumber = number1 
     return largetNumber
 }
